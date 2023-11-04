5d4f2b2dcaf3a921465a751dbb0bcad02cb8c6e6
Ifeoluwa <deleoyeroifeoluwa@gmail.com>
2023-11-04 20:48:11 +0100

    Coming soon Page

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ComingSoon from "./ComingSoon";
+
+function App() {
+  return (
+    <div className="App w-full">
+    <ComingSoon />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ComingSoon.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import backgroundImage from './assets/Background.jpg';
+import Logo from './assets/Bezal Logo.png';
+import BezalImage from './assets/In Construction.png';
+
+const ComingSoon = () => {
+  return (
+    <div
+      className="w-full h-screen p-2 flex flex-col items-center justify-start bg-contain bg-no-repeat md:bg-center"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+      }}
+    >
+      <div className="mt-10"> 
+        <img src={Logo} alt="" className="w-[20rem]" /> 
+      </div>
+      <div className="text-black text-center mt-4"> 
+        <h1 className=" font-medium md:text-2xl tracking-wide">This Website is Under Construction</h1>
+        <p className="text-[#828282] tracking-widest">Stay tuned for something amazing</p>
+      </div>
+      <div className="mt-10"> 
+        <img src={BezalImage} alt="" className="w-[40rem]" /> 
+      </div>
+      <div className="text-center mt-4">
+        <p className="text-[#828282] font-medium tracking-widest">Contact us: <span className='text-black'>info@bezalhomes.ng</span> |<span className='text-black'> T: +234 (0) 817 8206002</span></p>
+      </div>
+    </div>
+  );
+};
+
+export default ComingSoon;
