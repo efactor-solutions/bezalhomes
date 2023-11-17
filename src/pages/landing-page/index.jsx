c3962ec706bff24eec1a10e0e4aef5151fd060c8
Ifeoluwa <deleoyeroifeoluwa@gmail.com>
2023-11-17 11:12:09 +0100

    To make fixes to the page while this is up

--- src/pages/landing-page/index.jsx
+++ src/pages/landing-page/index.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import Navbar from "../../components/layouts/Navbar";
-import Header from "./header-page";
-import About from "./about";
-import MasterPieces from "./master-pieces";
-import ChiscoCourt from "./chisco-court";
-import Partners from "./partners";
-import Rehoboth from "./rehoboth";
-import Blog from "./blog";
-import Footer from "../../components/layouts/Footer";
+import ComingSoon from "../../ComingSoon";
+// import Navbar from "../../components/layouts/Navbar";
+// import Header from "./header-page";
+// import About from "./about";
+// import MasterPieces from "./master-pieces";
+// import ChiscoCourt from "./chisco-court";
+// import Partners from "./partners";
+// import Rehoboth from "./rehoboth";
+// import Blog from "./blog";
+// import Footer from "../../components/layouts/Footer";
 
 const LandingPage = () => {
   return (
     <div>
-      <Navbar />
+      <div>
+        <ComingSoon />
+      </div>
+      {/* <Navbar />
       <Header />
       <About />
       <MasterPieces />
@@ -20,7 +24,7 @@ const LandingPage = () => {
       <Partners />
       <Rehoboth />
       <Blog />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 };
